70d345e8b6fb0aec64ce840d013070a899ac143d
ELHACHESALTA <jfh1163@gmail.com>
2022-10-02 13:48:09 -0300

    Creación del Repositorio

---
+++ Introducción a la Programación/TP6 - Repetitivas/ejercicio1(1).php
@@ -0,0 +1,14 @@
+<?php
+	/* PROGRAMA Principal */
+	/* Muestra en pantalla los números pares menores o iguales al número ingresado */
+    /* int $numero */
+    echo "Ingrese un número: ";
+	$numero = trim(fgets(STDIN));
+	if ($numero % 2 <> 0) {
+		$numero = $numero - 1;
+	}
+	while (0 < $numero) {
+		echo $numero . "\n";
+		$numero = $numero - 2;
+	}
+?>
