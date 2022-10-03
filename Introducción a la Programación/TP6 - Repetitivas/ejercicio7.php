c90bbbc23e8bdae7a8c198c6d436234b0085d9c2
ELHACHESALTA <jfh1163@gmail.com>
2022-10-03 19:39:23 -0300

    Resolución TP6 hasta Ej7

---
+++ Introducción a la Programación/TP6 - Repetitivas/ejercicio7.php
@@ -0,0 +1,16 @@
+<?php
+    /* PROGRAMA Principal */
+	/* Muestra la suma de los números impares naturales que hay entre dos números ingresados. */
+	/* int $num1, $num2, $suma, $i */
+	echo "Nro minimo: ";
+	$num1 = trim(fgets(STDIN));
+    echo "Nro maximo: ";
+	$num2 = trim(fgets(STDIN));
+    $suma = 0;
+	for ($i = $num1; $i < $num2; $i++) {
+		if ($i % 2 == 1) {
+            $suma = $suma + $i;
+        }
+    }
+    echo "La suma es: " . $suma;
+?>
