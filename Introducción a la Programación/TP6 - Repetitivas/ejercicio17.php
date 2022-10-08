da72aa544d30b31cd31caadc0787e87fd20e9cfc
ELHACHESALTA <jfh1163@gmail.com>
2022-10-07 23:53:30 -0300

    Resolución TP6 hasta Ej17

---
+++ Introducción a la Programación/TP6 - Repetitivas/ejercicio17.php
@@ -0,0 +1,16 @@
+<?php
+    /* PROGRAMA Principal */
+    /* Comprueba y muestra la cantidad de vocales a partir de una secuencia de letras */
+    /* string $letra */
+    /* int $vocales */
+    $vocales = 0;
+    do {
+        echo "Ingrese letra (- para finalizar): ";
+        $letra = trim(fgets(STDIN));
+        if ($letra == "a" || $letra == "e" || $letra == "i" || $letra == "o" || $letra == "u") {
+            $vocales = $vocales + 1;
+        }
+    } while ($letra <> "-");
+    echo "\n";
+    echo "Cant. de vocales: " . $vocales;
+?>
