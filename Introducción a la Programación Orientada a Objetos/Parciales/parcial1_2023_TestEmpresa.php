c66799460e7721d2dbe003f0aa52e629e5fa4d06
Federico Herrera <jfh1163@gmail.com>
2023-05-31 21:47:28 -0300

    Resolución Parciales (IPOO)

--- Introducción a la Programación Orientada a Objetos/Parciales/parcial1_2023_TestEmpresa.php
+++ Introducción a la Programación Orientada a Objetos/Parciales/parcial1_2023_TestEmpresa.php
@@ -12,8 +12,8 @@
 
     //2 - Se crean dos instancias de la clase Bicicletas
 
-    $objBici1 = new Bicicleta (11, 89500, 2022, "Fire Bird Plegable Cuadro Acero", 85, true);   //165575 // TOTAL: 909575
-    $objBici2 = new Bicicleta (12, 310000, 2021, "Bicicleta Trek Marlin 5 Rodado 29 Talle L", 70, true); // 744000
+    $objBici1 = new Bicicleta (11, 89500, 2022, "Fire Bird Plegable Cuadro Acero", 85, true);
+    $objBici2 = new Bicicleta (12, 310000, 2021, "Bicicleta Trek Marlin 5 Rodado 29 Talle L", 70, true);
     $objBici3 = new Bicicleta (13, 10000, 2023, "Bicicleta Topmega Fixie Streeter R28 Acero 1vel Azul Osc T54", 55, false);
 
     //3 - Inciso 3 no existe
